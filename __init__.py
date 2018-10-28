93215485c800db261b4e79678211f15ff142a2f4
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-29 00:59:41 +0100

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .stock import *
-
+from . import stock
 
 def register():
     Pool.register(
-        ExternalReceptionLine,
+        stock.ExternalReceptionLine,
         module='stock_external_reception_lot', type_='model')
